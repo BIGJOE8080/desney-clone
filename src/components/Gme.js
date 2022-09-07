712f6bb83b7aa5f5c6ac667cf2cdb806b073593f
bigjoe8080 <joeoluwafemi80@gmail.com>
2022-09-07 22:10:58 +0100

    desney

---
+++ src/components/Gme.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import styled from 'styled-components'
+
+function Gme() {
+  return (
+        <Container >
+            Home
+        </Container>
+
+    )
+}
+
+export default Gme
+
+const Container = styled.min`
+    min-height: calc(100vh - 70px);
+    padding: 0 calc(3.5vw + 5px);`
